3acf97bad164cf0cd698b45abe231d4a8220d6b0
macbookair <macbookair@macbookairs-MacBook-Air.local>
2026-01-22 22:35:03 +0600

    update

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173','https://squadlog.up.railway.app','https://squadlog-console.up.railway.app'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://squadlog.up.railway.app', 'https://squadlog-console.up.railway.app', 'https://squadcart-console.up.railway.app', 'https://squadcart-console.up.railway.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
     credentials: true,
   });
-  
+
   // Serve static files from uploads directory
   app.useStaticAssets(join(process.cwd(), 'uploads'), {
     prefix: '/uploads/',
